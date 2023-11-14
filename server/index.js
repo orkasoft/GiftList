924b55b4c9e49c8ca4ce1d9c7d08281360ca5d32
emreordukaya <emreordukaya@hotmail.com>
2023-11-15 00:22:55 +0300

    Markle Tree Sample

--- server/index.js
+++ server/index.js
@@ -8,14 +8,17 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '6a9d9a950b493242eaadfe3735487ff2d300506cc884f42c6834e1caf053ee63';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
+  const name = body.name;
+  const proof = body.proof;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
